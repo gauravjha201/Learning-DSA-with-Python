16a59776f9c3437c0744435a0b230c71b04e22b6
gauravjha201 <jhagaurav034@gmail.com>
2026-01-15 11:19:01 +0530

    py

---
+++ prime_numbers.py
@@ -0,0 +1,19 @@
+
+def is_prime(n):
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+    return True
+
+def get_prime(num):
+    prime_nums=[]
+    for i in range(2,num+1):
+        if is_prime(i):
+            prime_nums.append(i)
+    return prime_nums
+
+print(get_prime(20))
+
+
+
+
